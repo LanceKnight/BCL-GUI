3e33ea50b2fe96bcffeda8850c21faffa93faa52
LanceKnight <lanceknight26@gmail.com>
2019-07-09 14:52:15 -0500

    communication between main process and renderer process is done

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-const {app, BrowserWindow} = require('electron');
+const {app, BrowserWindow, ipcMain} = require('electron');
+const cpp_connector = require('./src/cppsrc/CPPconnector/build/Release/CPPconnector.node');
 
 
 let mainWindow;
@@ -8,15 +9,22 @@ function createWindow () {
 		width: 1000,
 		height: 800,
 		webPreferences: {
-			nodeIntegration: false
+			nodeIntegration: true
 		}
 	});
 	
+
 	//open DevTool if needed
 	mainWindow.webContents.openDevTools();
 	
 	mainWindow.loadURL(`file://${__dirname}/src/index.html`);
 };
+
+function getFromCpp(){
+	module.exports = cpp_connector;
+	cpp_output = cpp_connector.hello();
+	return cpp_output;
+}
 
 app.on('ready', createWindow);
 
@@ -27,3 +35,13 @@ app.on('window-all-closed', () => {
 	  app.quit()
 	}
 });
+
+
+ipcMain.on('signalToMain', (event, arg)=> {
+	console.log('main process receiced signal from renderer process, msg:'+arg);
+	
+	var output = getFromCpp();
+	console.log('main process received output from c++, msg:' +output);
+
+	event.reply('signalToRenderer', output)
+})
--- src/cppsrc/CPPconnector/main_CPPconnector.cpp
+++ src/cppsrc/CPPconnector/main_CPPconnector.cpp
@@ -1,9 +1,39 @@
 #include <napi.h>
 
+std::string hello(){
+  return "Hello World";
+}
 
-Napi::Object InitAll(Napi::Env env, Napi::Object exports) {
+Napi::String HelloWrapped(const Napi::CallbackInfo& info) 
+{
+  Napi::Env env = info.Env();
+  Napi::String returnValue = Napi::String::New(env, hello());
+  
+  return returnValue;
+}
+
+Napi::Object Init(Napi::Env env, Napi::Object exports) 
+{
+  exports.Set(
+"hello", Napi::Function::New(env, HelloWrapped)
+  );
+ 
   return exports;
 }
+
+Napi::Object InitAll(Napi::Env env, Napi::Object exports) {
+  return Init(env, exports);
+}
+
+
+
+
+
+
+
+
+
+
 
 
 
